b5c723e497ad8d9875a80ad2553668908c622059
ajurje6 <antola.lukic@gmail.com>
2026-05-08 18:37:37 +0200

    fix bug in neuron.py, step_mode=m

--- modules/neuron.py
+++ modules/neuron.py
@@ -2,7 +2,7 @@ from spikingjelly.activation_based.neuron import IFNode, LIFNode
 
 class IF(IFNode):
     def __init__(self, surrogate_function=None,v_threshold =1., tau=None):
-        super().__init__(v_threshold=v_threshold, v_reset=None, surrogate_function=surrogate_function, detach_reset=False, step_mode='m',
+        super().__init__(v_threshold=v_threshold, v_reset=None, surrogate_function=surrogate_function, detach_reset=False, step_mode='s',
                          backend='torch', store_v_seq=False)
     def neuronal_fire(self):
         return self.surrogate_function()(self.v)
@@ -11,7 +11,7 @@ class LIF(LIFNode):
     def __init__(self,surrogate_function=None,v_threshold =1., tau=2.):
         super().__init__(tau = tau, decay_input= True, v_threshold = v_threshold,
                  v_reset = None, surrogate_function = surrogate_function,
-                 detach_reset = False, step_mode='m', backend='torch', store_v_seq = False)
+                 detach_reset = False, step_mode='s', backend='torch', store_v_seq = False)
 
     def neuronal_fire(self):
         return self.surrogate_function()(self.v)
